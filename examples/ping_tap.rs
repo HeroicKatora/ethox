e0cbbbe29edcda490f896dd3b86955fc90beac7e
Andreas Molzer <andreas.molzer@gmx.de>
2019-06-22 21:30:39 +0200

    Try an arp announcement

--- examples/ping_tap.rs
+++ examples/ping_tap.rs
@@ -6,6 +6,9 @@ use ethox::nic::{Device, TapInterface};
 use ethox::layer::{eth, ip, icmp};
 use ethox::wire::{Ipv4Cidr, EthernetAddress};
 
+// Only used as `arp` layer replacement.
+use ethox::wire::{Ipv4Address, EthernetProtocol, PayloadMut};
+
 fn main() {
     let Config {
         name,
@@ -30,6 +33,15 @@ fn main() {
 
     let mut interface = TapInterface::new(&name, vec![0; 1 << 14])
         .expect("Couldn't initialize interface");
+
+    // While arp is not done, send one gratuitous announcement.
+    // https://tools.ietf.org/html/rfc5944#section-4.6
+    interface.tx(1, eth.send(ArpAnnouncement {
+        addr: host.address(),
+    })).expect("Arp announcement failed");
+
+    eprintln!("Announced ourselves");
+
     loop {
         // Receive the next packet.
         let result = interface.rx(1, eth.recv(ip.recv(icmp.answer())));
@@ -47,4 +59,38 @@ struct Config {
     hostmac: EthernetAddress,
     gateway: Ipv4Cidr,
     gatemac: EthernetAddress,
+}
+
+struct ArpAnnouncement {
+    addr: Ipv4Address,
+}
+
+impl<P: PayloadMut> eth::Send<P> for ArpAnnouncement {
+    fn send(&mut self, mut raw: eth::RawPacket<P>) {
+        let src_addr = raw.handle.src_addr();
+        let mut prepared = raw.prepare(eth::Init {
+            src_addr,
+            dst_addr: EthernetAddress::BROADCAST,
+            ethertype: EthernetProtocol::Arp,
+            payload: 28,
+        }).unwrap();
+
+        {
+            // Send an arp request where sender = target
+            let slice = prepared
+                .payload_mut_slice();
+            slice[0..2].copy_from_slice(&[0, 1]); // HTYPE
+            slice[2..4].copy_from_slice(&[0x80, 0]); // PTYPE
+            slice[4..6].copy_from_slice(&[6, 4]); // H and P address length
+            slice[6..8].copy_from_slice(&[0, 1]); // Operation
+            slice[ 8..14].copy_from_slice(src_addr.as_bytes()); // H addr sender
+            slice[18..24].copy_from_slice(&[0; 6]); // H addr target
+            slice[14..18].copy_from_slice(self.addr.as_bytes()); // P addr sender
+            slice[24..28].copy_from_slice(self.addr.as_bytes()); // P addr target
+        }
+
+        prepared
+            .send()
+            .unwrap();
+    }
 }
