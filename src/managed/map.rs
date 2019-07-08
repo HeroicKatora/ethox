d1a327b8ed3c441f6b7252123cd29cb3506d92ec
HeroicKatora <andreas.molzer@gmx.de>
2019-07-08 17:16:28 +0200

    Abstract entry as basic generic map primitive
    We can build get_mut and insert on-top of entry. Since there are both
    owning and non-owning, Copy and non-Copy version we have to take some
    care to allow insertion and removal which may require cloning in one of
    those cases. Creating an Entry-api makes it possible to split these into
    another enum if required.

--- src/managed/map.rs
+++ src/managed/map.rs
@@ -21,3 +21,90 @@ pub enum Map<'a, K: Ord, V> {
     Btree(btree_map::BTreeMap<K, V>),
 }
 
+/// An entry of the map.
+///
+/// Can be inspected, filled, or removed depending on its state variant.
+pub enum Entry<'map, 'a, K: Ord, V> {
+    Occupied(OccupiedEntry<'map, 'a, K, V>),
+    Vacant(VacantEntry<'map, 'a, K, V>),
+    Full,
+}
+
+pub struct OccupiedEntry<'map, 'a, K: Ord, V> {
+    inner: Occupied<'map, 'a, K, V>,
+}
+
+enum Occupied<'map, 'a, K, V> {
+    Pairs {
+        list: &'map mut List<'a, (K, V)>,
+        index: usize,
+    },
+    Btree(btree_map::OccupiedEntry<'map, K, V>),
+}
+
+pub struct VacantEntry<'map, 'a, K: Ord, V> {
+    inner: Vacant<'map, 'a, K, V>,
+}
+
+enum Vacant<'map, 'a, K, V> {
+    Pairs {
+        list: &'map mut List<'a, (K, V)>,
+    },
+    Btree(btree_map::VacantEntry<'map, K, V>),
+}
+
+
+impl<K: Ord, V> Map<'_, K, V> {
+    pub fn get(&self, key: &K) -> Option<&V> {
+        match self {
+            Map::Pairs(list) => list
+                .as_slice()
+                .iter()
+                .find(|(k, _)| k == key)
+                .map(|(_, val)| val),
+            Map::Btree(tree) => tree.get(key),
+        }
+    }
+}
+
+impl<'a, K: Ord, V> Map<'a, K, V> {
+    pub fn entry(&mut self, key: K) -> Entry<'_, 'a, K, V> {
+        match self {
+            Map::Pairs(list) => {
+                let index = list
+                    .as_slice()
+                    .iter()
+                    .position(|(k, _)| k == &key);
+                match index {
+                    Some(index) => Entry::Occupied(OccupiedEntry {
+                        inner: Occupied::Pairs {
+                            list,
+                            index,
+                        },
+                    }),
+                    None => Entry::Vacant(VacantEntry {
+                        inner: Vacant::Pairs {
+                            list,
+                        },
+                    }),
+                }
+            },
+            Map::Btree(tree) => tree.entry(key).into(),
+        }
+    }
+}
+
+impl<'a, K: Ord, V>
+    From<btree_map::Entry<'a, K, V>>
+for Entry<'a, '_, K, V> {
+    fn from(entry: btree_map::Entry<'a, K, V>) -> Self {
+       match entry {
+           btree_map::Entry::Occupied(occ) => Entry::Occupied(OccupiedEntry {
+               inner: Occupied::Btree(occ),
+           }),
+           btree_map::Entry::Vacant(vac) => Entry::Vacant(VacantEntry {
+               inner: Vacant::Btree(vac),
+           }),
+       }
+    }
+}
