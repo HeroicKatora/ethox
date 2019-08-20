89fbb90b34f268f4ddeb0b0b580e5eac734ff242
Andreas Molzer <andreas.molzer@gmx.de>
2019-08-20 05:49:09 +0200

    Add a full no_std example
    The goal is to experiment with the size of its binary and to minimize
    that as far as possible.

---
+++ ethox-no-std/main.rs
@@ -0,0 +1,44 @@
+//! Example for raw usage without `std` or `alloc`.
+//!
+//! Note that this is not as ergonomic for argument parsing, so we set up a stack answering only
+//! icmpv4 pings (and arp) without any routes.
+#![no_std]
+
+use ethox::managed::Slice;
+use ethox::nic::{Device, TapInterface};
+use ethox::layer::{eth, ip, icmp};
+use ethox::wire::{Ipv4Address, Ipv4Cidr, EthernetAddress};
+
+fn main() {
+    let name = "tap0";
+    let host = Ipv4Cidr::new(Ipv4Address([10, 0, 0, 1]), 24);
+    let hostmac = EthernetAddress([0xab,0xff,0xff,0xff,0xff,0xff]);
+
+    let mut eth = eth::Endpoint::new(hostmac);
+
+    let mut neighbors = [eth::Neighbor::default(); 10];
+    let mut ip = ip::Endpoint::new(Slice::One(host.into()),
+        // No routes at all
+        ip::Routes::new(Slice::empty()), 
+        // But do automatic arp
+        eth::NeighborCache::new(&mut neighbors[..]));
+
+    let mut icmp = icmp::Endpoint::new();
+
+    let mut buffer = [0; 1 << 14];
+    let mut interface = TapInterface::new(&name, &mut buffer[..])
+        .expect("Couldn't initialize interface");
+
+    loop {
+        // Receive the next packet.
+        let rx_ok = interface.rx(1, eth.recv(ip.recv(icmp.answer())));
+        // Give some chance for outgoing maintenance such as arp.
+        let tx_ok = interface.tx(1, eth.send(ip.layer_internal()));
+
+        let result = rx_ok.and_then(|x| tx_ok.map(|y| x + y));
+
+        if result.is_err() {
+            break;
+        }
+    }
+}
