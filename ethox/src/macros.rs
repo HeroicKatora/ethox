2953229dbdd9d2fd6fe20f5bb551aef7bf8cffe0
HeroicKatora <andreas.molzer@gmx.de>
2019-11-04 13:36:43 +0100

    Allow doc items on all variants
    I see no good reason to require an extra specifier just to show public
    docs. It should be simple to achieve the opposite on single items in
    fewer lines instead.

--- ethox/src/macros.rs
+++ ethox/src/macros.rs
@@ -24,13 +24,13 @@ macro_rules! enum_with_unknown {
     (
         $( #[$enum_attr:meta] )*
         pub enum $name:ident($ty:ty) {
-            $( $variant:ident = $value:expr ),+ $(,)*
+            $($(#[$val_attr:meta])* $variant:ident = $value:expr ),+ $(,)*
         }
     ) => {
         enum_with_unknown! {
             $( #[$enum_attr] )*
             pub doc enum $name($ty) {
-                $( #[doc(shown)] $variant = $value ),+
+                $($(#[$val_attr])* #[doc(shown)] $variant = $value ),+
             }
         }
     };
