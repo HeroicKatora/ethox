bc8b0ac8a34303af31076cf77eda346f28010b69
Andreas Molzer <andreas.molzer@gmx.de>
2020-10-16 19:32:07 +0200

    Basic setup for crypto and state

--- ethox-wg/src/lib.rs
+++ ethox-wg/src/lib.rs
@@ -1,7 +1,41 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+//! A `no_std` Wireguard implementation with `ethox`.
+//!
+//! It's not no-alloc since the underlying crypto implementation is not. If you find a library as
+//! good as `ring` which provides this then we might consider switching.
+#![no_std]
+use core::time::Duration;
+
+use ethox::time::Instant;
+use ring::aead;
+
+pub struct Peer {
+    /// Send empty packet if we haven't heard for a while.
+    keepalive: Duration,
+    /// The last time we heard from the peer.
+    last_alive: Instant,
+    /// The constant rekey timeout.
+    /// If keepalive times out then we initiate rekey handshake.
+    rekey_timeout: Duration,
+    /// The REJECT_AFTER_TIME constant for the peer.
+    reject_after_time: Duration,
+    /// The REKEY_AFTER_MESSAGES constant.
+    /// After this number of packets we initiate a new handshake.
+    rekey_after_messages: u32,
+    /// The number of message we sent without rekey according to `rekey_after_messages`.
+    /// Begin to drop packets when this is greater than `rekey_after_messages`.
+    messages_without_rekey: u32,
+    /// Timeout until we rekey independent of messages or keep-alive.
+    rekey_after_time: Duration,
+    /// The last timestamp when we rekeyed.
+    last_rekey_time: Instant,
+}
+
+/// State of one handshake.
+///
+/// Retried after `REKEY_TIMEOUT + jitter` where jitter `[0; 330ms]`.
+pub struct Handshake {
+    /// The constant rekey timeout.
+    rekey_timeout: Duration,
+    /// Currently chosen jitter duration.
+    jitter: Duration,
 }
