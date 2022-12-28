55a99e6c1f69166a1237b0964baf71558f22f915
Andreas Molzer <andreas.molzer@gmx.de>
2022-12-28 05:07:29 +0100

    AF-XDP: Concrete plan for  buffer controller

--- ethox-afxdp/src/lib.rs
+++ ethox-afxdp/src/lib.rs
@@ -2,10 +2,10 @@
 extern crate alloc;
 
 use afxdp::socket::{Socket, SocketRx, SocketTx};
-use afxdp::umem::{UmemCompletionQueue, UmemFillQueue};
+use afxdp::umem::{UmemCompletionQueue, UmemFillQueue, UmemError};
 use afxdp::PENDING_LEN;
 use afxdp::{buf_mmap::BufMmap, buf_pool_vec::BufPoolVec};
-use alloc::boxed::Box;
+use alloc::{boxed::Box, vec::Vec};
 
 use arraydeque::{ArrayDeque, Wrapping};
 
@@ -23,20 +23,98 @@ pub struct AfXdp {
     link_fq: UmemFillQueue<'static, BufTag>,
     link_cq: UmemCompletionQueue<'static, BufTag>,
 
-    // the temporary pool of mapped buffers received.
-    pending_rx: ArrayDeque<[BufMmap<'static, BufTag>; PENDING_LEN], Wrapping>,
-    // the temporary pool of mapped buffers to fill.
-    pending_fx: ArrayDeque<[BufMmap<'static, BufTag>; PENDING_LEN], Wrapping>,
-    // the temporary pool of mapped buffers to transmit.
+    /// free buffers with no assigned use.
+    free: Vec<BufMmap<'static, BufTag>>,
+    /// the temporary pool of mapped buffers received.
+    queue_rx: ArrayDeque<[BufMmap<'static, BufTag>; PENDING_LEN], Wrapping>,
+    /// the temporary pool of mapped buffers ready to add to fill queue.
+    pending_fx: Vec<BufMmap<'static, BufTag>>,
+    /// the temporary pool of mapped buffers ready to fill with transmit data.
+    queue_tx: Vec<BufMmap<'static, BufTag>>,
+    /// Buffers enqueued for transmit in order.
     pending_tx: ArrayDeque<[BufMmap<'static, BufTag>; PENDING_LEN], Wrapping>,
+
     // A buffer of handles we use temporarily to communicate with the sender and receiver.
     handles: Box<[Handle]>,
+
+    /// Gathered statistics about socket usage, buffer usage, etc.
+    stats: Box<Stats>,
+
+    /// Number of packet buffers to reserve for receive.
+    watermark_rx: usize,
+    /// Number of packet buffers to reserve for transmission.
+    watermark_tx: usize,
+}
+
+#[derive(Default)]
+struct Stats {
 }
 
 #[repr(transparent)]
 pub struct Buffer(BufMmap<'static, BufTag>);
 
 pub struct Handle {}
+
+/// Buffer management operations.
+///
+/// All operations should ensure our local liveness properties:
+/// * Assuming the network card will eventually transmit queued packet and put them into the
+///   completion queue then:
+/// * Eventually some buffers are available for the fill queue (i.e. currently inserted or in the
+///   `pending_fx` buffer).
+/// * Eventually a call to `tx` can enqueue at least one buffer.
+///
+/// This is maintained by treating rx&tx as two separate systems of buffers, with some auxiliary
+/// buffers floating freely between the two. This is _not_ optimal for latency. The pending
+/// transmit queues might still grow large of receive packets sparse if all floating buffers are in
+/// one of the two systems (especially if multiple sockets are involved). But real-time with small
+/// guarantees is better than none.
+///
+/// It follows that, in the current implementation, the total number of buffers should not exceed
+/// `PENDING_LEN` as this many buffers could be put into any queue. Packets would need to be
+/// assigned as free floating buffers otherwise, dropping any egress or ingress content they have.
+/// Not incorrect, but very inconvenient.
+///
+/// Invariants to uphold everywhere:
+/// * Number of buffers in `free`, `pending_fx`, and the fill queue is at least `watermark_rx`.
+/// * Number of buffers in `free`, `pending_tx`, and the completion queue is at least `watermark_tx`.
+impl AfXdp {
+    /// Cleanup the receive queue.
+    ///
+    /// Other packets are set aside into our fill queue. Note: sending as an 'instant response' is
+    /// not guaranteed! The implementation should inspect the handles in a smarter way to avoid us
+    /// dropping a retransmit here.
+    ///
+    /// Postcondition:
+    /// * `queue_rx` is empty.
+    fn clean_rx(&mut self, num: usize) -> Result<(), UmemError> {
+        todo!()
+    }
+
+    /// Cleanup the transmit queue according to handles.
+    ///
+    /// Other packets are set aside into our fill queue.
+    /// * `queue_tx` is empty.
+    fn enqueue_tx(&mut self, num: usize) {
+        todo!()
+    }
+
+    /// Gather some free packet buffers into the fill queue.
+    ///
+    /// Call this eventually after buffers from the fill queue have been consumed. Never consumes
+    /// too many packets to starve the transmit queue.
+    fn fill_fq(&mut self) -> usize {
+        todo!()
+    }
+
+    /// Gather some free packet buffers into the transmit queue.
+    ///
+    /// Call this eventually after buffers are accepted for transmit. Never consumes too many
+    /// packets to starve the fill queue.
+    fn fill_tx(&mut self) -> usize {
+        todo!()
+    }
+}
 
 impl Device for AfXdp {
     type Payload = Buffer;
@@ -53,7 +131,7 @@ impl Device for AfXdp {
         mut sender: impl ethox::nic::Send<Self::Handle, Self::Payload>,
     ) -> ethox::layer::Result<usize> {
         let packets = self
-            .pending_rx
+            .queue_tx
             .iter_mut()
             .zip(&mut self.handles[..])
             .take(max)
@@ -73,13 +151,13 @@ impl Device for AfXdp {
         mut receiver: impl ethox::nic::Recv<Self::Handle, Self::Payload>,
     ) -> ethox::layer::Result<usize> {
         let max = self.handles.len().min(max);
-        let count = match self.rx.try_recv(&mut self.pending_rx, max, BufTag) {
+        let count = match self.rx.try_recv(&mut self.queue_rx, max, BufTag) {
             Ok(count) => count,
             Err(afxdp::socket::SocketError::Failed) => return Err(ethox::layer::Error::Exhausted),
         };
 
         let packets = self
-            .pending_rx
+            .queue_rx
             .iter_mut()
             .zip(&mut self.handles[..])
             .map(|(payload, handle)| ethox::nic::Packet{
