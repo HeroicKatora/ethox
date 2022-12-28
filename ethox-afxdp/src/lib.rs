3b86e57f9431a648d894d44cc6dff46f5e0580aa
Andreas Molzer <andreas.molzer@gmx.de>
2022-12-29 00:37:04 +0100

    Implement basic buffer management
    Somehow this could be more testable. What I'd really like to have was
    some way to _simulate_ our queueing regime and thus evaluate the queue
    growth/latency and other behavior for kernel-NIC scenarios.

--- ethox-afxdp/src/lib.rs
+++ ethox-afxdp/src/lib.rs
@@ -1,10 +1,10 @@
 #![no_std]
 extern crate alloc;
 
+use afxdp::buf_mmap::BufMmap;
 use afxdp::socket::{Socket, SocketRx, SocketTx};
-use afxdp::umem::{UmemCompletionQueue, UmemFillQueue, UmemError};
+use afxdp::umem::{UmemCompletionQueue, UmemError, UmemFillQueue};
 use afxdp::PENDING_LEN;
-use afxdp::{buf_mmap::BufMmap, buf_pool_vec::BufPoolVec};
 use alloc::{boxed::Box, vec::Vec};
 
 use arraydeque::{ArrayDeque, Wrapping};
@@ -18,42 +18,66 @@ pub struct BufTag;
 pub struct AfXdp {
     rx: SocketRx<'static, BufTag>,
     tx: SocketTx<'static, BufTag>,
+    #[allow(dead_code)]
     sock: Socket<'static, BufTag>,
 
     link_fq: UmemFillQueue<'static, BufTag>,
     link_cq: UmemCompletionQueue<'static, BufTag>,
 
-    /// free buffers with no assigned use.
-    free: Vec<BufMmap<'static, BufTag>>,
     /// the temporary pool of mapped buffers received.
-    queue_rx: ArrayDeque<[BufMmap<'static, BufTag>; PENDING_LEN], Wrapping>,
-    /// the temporary pool of mapped buffers ready to add to fill queue.
-    pending_fx: Vec<BufMmap<'static, BufTag>>,
+    queue_rx: Vec<BufMmap<'static, BufTag>>,
     /// the temporary pool of mapped buffers ready to fill with transmit data.
     queue_tx: Vec<BufMmap<'static, BufTag>>,
+
+    /// free buffers with no assigned use.
+    free: Vec<BufMmap<'static, BufTag>>,
+    pending_rx: ArrayDeque<[BufMmap<'static, BufTag>; PENDING_LEN], Wrapping>,
+    /// the temporary pool of mapped buffers ready to add to fill queue.
+    pending_fq: Vec<BufMmap<'static, BufTag>>,
     /// Buffers enqueued for transmit in order.
     pending_tx: ArrayDeque<[BufMmap<'static, BufTag>; PENDING_LEN], Wrapping>,
+    pending_cq: Vec<BufMmap<'static, BufTag>>,
 
     // A buffer of handles we use temporarily to communicate with the sender and receiver.
     handles: Box<[Handle]>,
 
     /// Gathered statistics about socket usage, buffer usage, etc.
+    #[allow(dead_code)]
     stats: Box<Stats>,
 
     /// Number of packet buffers to reserve for receive.
     watermark_rx: usize,
     /// Number of packet buffers to reserve for transmission.
     watermark_tx: usize,
+
+    /// Packets currently in the RX system (>=watermark_rx).
+    current_rx: usize,
+    /// Packets currently in the TX system (>=watermark_tx).
+    current_tx: usize,
+
+    // Buffer tracking of free float.
+    /// Number of essential RX buffers in the free state.
+    essential_free_rx: usize,
+    /// Number of essential TX buffers in the free state.
+    essential_free_tx: usize,
 }
 
 #[derive(Default)]
-struct Stats {
-}
+struct Stats {}
 
 #[repr(transparent)]
 pub struct Buffer(BufMmap<'static, BufTag>);
 
-pub struct Handle {}
+pub struct Handle {
+    /// Should we send? TODO: allow more than one transmit queue.
+    send: Option<u16>,
+}
+
+pub struct IoReport {
+    pub egress: usize,
+    pub ingress: usize,
+    _inner: (),
+}
 
 /// Buffer management operations.
 ///
@@ -87,15 +111,86 @@ impl AfXdp {
     ///
     /// Postcondition:
     /// * `queue_rx` is empty.
-    fn clean_rx(&mut self, num: usize) -> Result<(), UmemError> {
-        todo!()
+    fn post_rx(&mut self) {
+        // How many packets can leave the RX loop? These all come from the fill queue.
+        let spill = self.current_rx.saturating_sub(self.watermark_rx);
+        let essential = self.current_rx - spill;
+        // Essential packets are reserved for RX tasks.
+        self.essential_free_rx += essential;
+        // All other packets leave the RX assignment and are floating again.
+        self.current_rx -= spill;
+
+        // None of the packets are allowed to get sent (these packets leave the RX system).
+        if spill == 0 {
+            self.free.extend(self.queue_rx.drain(..));
+            return;
+        }
+
+        let mut tx = 0;
+        for (packet, hdl) in self.queue_rx.drain(..).zip(&self.handles[..]) {
+            match hdl.send {
+                Some(0) if tx < spill => {
+                    tx += 1;
+                    self.pending_tx.push_back(packet);
+                }
+                _ => self.free.push(packet),
+            }
+        }
+
+        self.current_tx += tx;
     }
 
     /// Cleanup the transmit queue according to handles.
     ///
     /// Other packets are set aside into our fill queue.
     /// * `queue_tx` is empty.
-    fn enqueue_tx(&mut self, num: usize) {
+    fn post_tx(&mut self) {
+        let mut freed = 0;
+        for (packet, hdl) in self.queue_tx.drain(..).zip(&self.handles[..]) {
+            match hdl.send {
+                Some(0) => {
+                    self.pending_tx.push_back(packet);
+                }
+                _ => {
+                    freed += 1;
+                    self.free.push(packet);
+                }
+            }
+        }
+
+        // Number of essential packets freed.
+        let float = self.watermark_tx.saturating_sub(self.current_tx - freed);
+        self.current_tx -= freed;
+        self.essential_free_tx += float;
+    }
+
+    /// Accept some buffers from the `afxdp` receive buffer.
+    ///
+    /// Precondition:
+    /// `self.queue_rx` is empty.
+    fn pre_rx(&mut self, max: usize) -> usize {
+        let actual = self.pending_rx.len().min(max);
+        self.queue_rx.extend(self.pending_rx.drain(..actual));
+        actual
+    }
+
+    /// Prepare some buffers from the free floating buffer.
+    ///
+    /// Precondition:
+    /// `self.queue_rx` is empty.
+    fn pre_tx(&mut self, max: usize) -> usize {
+        debug_assert!(self.essential_free_rx + self.essential_free_tx <= self.free.len());
+        let actual = (self.free.len() - self.essential_free_rx).min(max);
+        self.essential_free_tx = self.essential_free_tx.saturating_sub(actual);
+        self.current_tx += actual;
+        debug_assert!(self.essential_free_rx + self.essential_free_tx <= self.free.len());
+
+        self.queue_rx.extend(self.pending_rx.drain(..actual));
+        actual
+    }
+
+    /// Gather packet buffers from the completion queue.
+    fn periodic_reap_cq(&mut self) -> usize {
         todo!()
     }
 
@@ -103,16 +198,41 @@ impl AfXdp {
     ///
     /// Call this eventually after buffers from the fill queue have been consumed. Never consumes
     /// too many packets to starve the transmit queue.
-    fn fill_fq(&mut self) -> usize {
-        todo!()
+    fn periodic_reap_fq(&mut self) -> usize {
+        let batch = 16;
+        debug_assert!(self.essential_free_rx + self.essential_free_tx <= self.free.len());
+        let actual = (self.free.len() - self.essential_free_tx).min(batch);
+        todo!();
+
+        self.pending_fq.extend(self.free.drain(..actual));
     }
 
-    /// Gather some free packet buffers into the transmit queue.
+    /// Handle fill and completion queue actions with the kernel.
     ///
-    /// Call this eventually after buffers are accepted for transmit. Never consumes too many
-    /// packets to starve the fill queue.
-    fn fill_tx(&mut self) -> usize {
-        todo!()
+    /// Run this periodically.
+    pub fn do_io(&mut self) -> Result<IoReport, UmemError> {
+        if self.tx.needs_wakeup() || self.link_fq.needs_wakeup() {
+            self.rx.wake();
+        }
+
+        let batch = 16;
+        let igress_err = self.rx.try_recv(&mut self.pending_rx, batch, BufTag);
+        let ogress_err = self.tx.try_send(&mut self.pending_tx, batch);
+
+        // Periodic maintenance tasks, the two kernel queues.
+        self.periodic_reap_fq();
+        let fill_err = self.link_fq.fill(&mut self.pending_fq, batch);
+        let complete_err = self.link_cq.service(&mut self.pending_cq, batch);
+        self.periodic_reap_cq();
+
+        let _ = igress_err.and(ogress_err);
+        let _ = fill_err.and(complete_err);
+
+        Ok(IoReport {
+            egress: 0,
+            ingress: 0,
+            _inner: (),
+        })
     }
 }
 
@@ -130,19 +250,22 @@ impl Device for AfXdp {
         max: usize,
         mut sender: impl ethox::nic::Send<Self::Handle, Self::Payload>,
     ) -> ethox::layer::Result<usize> {
+        let max = self.handles.len().min(max);
+        let count = self.pre_tx(max);
+
         let packets = self
             .queue_tx
             .iter_mut()
             .zip(&mut self.handles[..])
             .take(max)
-            .map(|(payload, handle)| ethox::nic::Packet{
+            .map(|(payload, handle)| ethox::nic::Packet {
                 handle,
                 payload: unsafe { core::mem::transmute::<_, &mut Buffer>(payload) },
             });
 
         sender.sendv(packets);
-
-        todo!()
+        self.post_tx();
+        Ok(count)
     }
 
     fn rx(
@@ -151,23 +274,20 @@ impl Device for AfXdp {
         mut receiver: impl ethox::nic::Recv<Self::Handle, Self::Payload>,
     ) -> ethox::layer::Result<usize> {
         let max = self.handles.len().min(max);
-        let count = match self.rx.try_recv(&mut self.queue_rx, max, BufTag) {
-            Ok(count) => count,
-            Err(afxdp::socket::SocketError::Failed) => return Err(ethox::layer::Error::Exhausted),
-        };
+        let count = self.pre_rx(max);
 
         let packets = self
             .queue_rx
             .iter_mut()
+            .take(max)
             .zip(&mut self.handles[..])
-            .map(|(payload, handle)| ethox::nic::Packet{
+            .map(|(payload, handle)| ethox::nic::Packet {
                 handle,
                 payload: unsafe { core::mem::transmute::<_, &mut Buffer>(payload) },
             });
 
         receiver.receivev(packets);
-        // FIXME: handle instantaneous retransmit.
-
+        self.post_rx();
         Ok(count)
     }
 }
@@ -199,10 +319,21 @@ impl PayloadMut for Buffer {
 
 impl ethox::nic::Handle for Handle {
     fn queue(&mut self) -> ethox::layer::Result<()> {
-        todo!()
+        self.send = Some(0);
+        Ok(())
     }
 
     fn info(&self) -> &dyn ethox::nic::Info {
+        self
+    }
+}
+
+impl ethox::nic::Info for Handle {
+    fn timestamp(&self) -> ethox::time::Instant {
         todo!()
+    }
+
+    fn capabilities(&self) -> ethox::nic::Capabilities {
+        ethox::nic::Capabilities::no_support()
     }
 }
